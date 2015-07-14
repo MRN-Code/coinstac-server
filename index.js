5c4bb88e162e34cb2742c1aefa235b833e6103b2
rsskmr <ex@ex.com>
2015-07-14 11:59:39 -0700

    register routes through dir, add config

--- index.js
+++ index.js
@@ -3,13 +3,7 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 const config = require('config');
-const plugins = [
-    {
-        register: require('./lib/plugins/hapi-pouch.js'),
-        options: config.get('pouchdb.users')
-    },
-    { register: require('inject-then') }
-];
+const glob = require('glob')
 
 server.connection({port:config.get('server.port')});
 
@@ -24,33 +18,59 @@ server.route({
         });
         /*
         const users = [
-            {
-                id: 1,
-                username: 'test',
-                email: 'test@test.com',
-                password: 'md5hash',
-                name: 'Test Name',
-                institution: 'testitution'
-            }
+        {
+        id: 1,
+        username: 'test',
+        email: 'test@test.com',
+        password: 'md5hash',
+        name: 'Test Name',
+        institution: 'testitution'
+        }
         ];
         reply(JSON.stringify(users));
         */
     }
 });
 
-server.register(plugins, function(err) {
-        if (err) {
-            console.log('Error registering plugins', err);
-        } else {
-            console.log('plugins registered');
-            if (!module.parent) {
-                server.start(() => {
-                    console.log('Server running at:', server.info.uri);
-                });
+/**
+* Generate a plugin array, including all route plugins under lib/app_routes
+* @return {array}
+*/
+setPlugins = function () {
+    const plugins = [
+        {
+            register: require('./lib/plugins/hapi-pouch.js'),
+            options: config.get('pouchdb.users')
+        },
+        { register: require('inject-then') }
+    ];
+
+    // add route plugins
+    var newRoute;
+    glob.sync('./lib/app_routes/*.js').forEach (function (file) {
+        newRoute = {
+            register: require(file),
+            options: {
+                redisClient: client,
+                relations: server.plugins.hapi_relations
             }
+        };
+        plugins.push(newRoute);
+    });
+    return plugins;
+};
+
+server.register(setPlugins(), function(err) {
+    if (err) {
+        console.log('Error registering plugins', err);
+    } else {
+        console.log('plugins registered');
+        if (!module.parent) {
+            server.start(() => {
+                console.log('Server running at:', server.info.uri);
+            });
         }
     }
-
-);
+});
 
 module.exports = server;
